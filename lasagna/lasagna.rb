61b44852ebffdb3c998e2c4ccca809f088c6cad2
ViniciosTI <0vinicios1@gmail.com>
2022-06-21 07:10:12 -0300

    lasagna preparation time

---
+++ lasagna/lasagna.rb
@@ -0,0 +1,19 @@
+class Lasagna
+  EXPECTED_MINUTES_IN_OVEN = 40
+
+  def remaining_minutes_in_oven(actual_minutes_in_oven)
+    return EXPECTED_MINUTES_IN_OVEN - actual_minutes_in_oven
+    # raise 'Please implement the Lasagna#remaining_minutes_in_oven method'
+  end
+
+  def preparation_time_in_minutes(layers)
+    return layers * 2
+    # raise 'Please implement the Lasagna#preparation_time_in_minutes method'
+  end
+
+  def total_time_in_minutes(number_of_layers:, actual_minutes_in_oven:)
+    preparetion_time = preparation_time_in_minutes(number_of_layers)
+    return preparetion_time + actual_minutes_in_oven
+    # raise 'Please implement the Lasagna#total_time_in_minutes method'
+  end
+end
